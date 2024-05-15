9232429528db1193ed2511994764a2e7a7bb8b95
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-15 10:24:58 +0530

    add hero section component

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+import Hero from "./components/Hero"
+
+const Home = () => {
+    return (
+        <div>
+            <Hero />
+        </div>
+    )
+}
+
+export default Home
