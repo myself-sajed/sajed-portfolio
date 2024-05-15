eef182d03a11701473c0db59dfda7e1889a63759
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-15 22:05:31 +0530

    encapsulated github and linked action

---
+++ src/app/(home)/components/LinkActions.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const LinkActions = () => {
+    return (
+        <div className='flex items-center justify-center gap-5 mt-24'>
+            <Link href="https://github.com/myself-sajed">
+                <Image draggable={false} src="/assets/github.svg" alt="github" width={58} height={58} /></Link>
+            <Link href="https://www.linkedin.com/in/shaikhsajed">
+                <Image draggable={false} src="/assets/linkedin.svg" alt="linkedin" width={50} height={50} />
+            </Link>
+        </div>
+    )
+}
+
+export default LinkActions
