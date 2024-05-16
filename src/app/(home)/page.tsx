41bc6d4703a7753e7b082a16448bf22031d37f53
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-16 15:15:40 +0530

    add experience projects and contact components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,12 +1,15 @@
+import Contact from "./components/Contact"
+import Experience from "./components/Experience"
 import Hero from "./components/Hero"
+import Projects from "./components/Projects"
 
 const Home = () => {
     return (
         <div className="w-full">
             <Hero />
-            <div className="h-screen">
-
-            </div>
+            <Experience />
+            <Projects />
+            <Contact />
         </div>
     )
 }
