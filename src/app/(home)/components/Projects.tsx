e47b4637f0995c8accd81dfa8ea6b0f2e70790c6
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-16 15:18:25 +0530

    add project

---
+++ src/app/(home)/components/Projects.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Projects = () => {
+    return (
+        <div id="projects" className='h-screen w-full'>
+            <h1 className="text-xl font-semibold text-center">
+                <code className="text-primary">{`<Major Projects />`}</code>
+            </h1>
+        </div>
+    )
+}
+
+export default Projects
